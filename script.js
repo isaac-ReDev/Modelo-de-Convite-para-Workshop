86a707d2e8d6666ab25efe18dcd8a3da75e83f36
isaac-ReDev <isacbs2017ce@gmail.com>
2022-07-07 18:44:06 -0300

    parei o setTimeout apos ele escrever tudo na ne tela. Adicionei a sections dos autores no html e fiz o esiilo no css.

--- script.js
+++ script.js
@@ -2,24 +2,23 @@ const h1 = document.querySelector('[data-js="typing-1"]');
 const h2 = document.querySelector('[data-js="typing-2"]');
 const h3 = document.querySelector('[data-js="typing-3"]');
 
-
 const hs = [h1,h2,h3];
-
 const message = ['>SEMANA INGLES PARA DEVS_', 'De 04 a 10 de Julho de 2022', '>Um evento gratuito focado em programadores que querem aprender inglês para mudar de nível na carreira.'];
+const intervalTypingText = setInterval(typingText, 100);
+
 
 let messageIndex = 0;
 let charactersIndex = 0;
 let currentMessage = '';
 let currentCharacters = '';
 let  i = 0;
-
-
+ 
 
 function typingText(){
     currentMessage       = message[messageIndex];
     currentCharacters    = currentMessage.slice(0, charactersIndex++);
     hs[i].innerHTML     = currentCharacters;
-
+    console.log('dss')
     if(currentCharacters.length === currentMessage.length ){
         messageIndex++;
         charactersIndex = 0;
@@ -27,5 +26,8 @@ function typingText(){
     }
 }
 
-setInterval(typingText, 50);
+setTimeout(function(){
+    console.log('sstoped');
+    clearInterval(intervalTypingText)
+},20000)
 
